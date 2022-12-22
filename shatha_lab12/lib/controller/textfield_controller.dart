b0f511ff9b03735ff4b70295833aa9319c8ac1fe
shathalrowaisan <shathalrowaisan@gmail.com>
2022-12-22 20:10:35 +0300

    shatha-lab12

---
+++ shatha_lab12/lib/controller/textfield_controller.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class TextFieldController extends GetxController{
+  
+  TextEditingController email = TextEditingController();
+  TextEditingController password = TextEditingController();
+  TextEditingController newEmail = TextEditingController();
+  TextEditingController name = TextEditingController();
+  TextEditingController age = TextEditingController();
+
+}
